b8f546153713ab2caa39c65dd679e78cfe13477c
ashishcw <59785028+ashishcw@users.noreply.github.com>
2024-02-25 12:17:46 +0530

    Base Engine Added
    This is a base engine, e.g. main loop, display settings etc.

---
+++ src/main/java/org/example/constant/Constants.java
@@ -0,0 +1,18 @@
+package org.example.constant;
+
+public class Constants {
+
+    //display settings
+    public static final int WINDOW_WIDTH = 800;
+    public static final int WINDOW_HEIGHT = 800;
+    public static final int FPS = 60;
+    public static final long OPTIMAL_TIME = 1000000000 / FPS;
+
+
+    public static final int NODE_SIZE = 10;
+    public static final int NODE_OFFSET = 10;
+
+    public static final int MAX_ROWS = 15;
+    public static final int MAX_COLS = 15;
+
+}
---
+++ src/main/java/org/example/display/Window.java
@@ -0,0 +1,38 @@
+package org.example.display;
+
+import org.example.Main;
+import org.example.constant.Constants;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Window {
+    private Main main;
+    private JFrame frame;
+
+    public Window(Main main) {
+        this.main = main;
+
+        init();
+    }
+
+    private void init(){
+        if(this.frame == null){
+            this.frame = new JFrame();
+        }
+
+        this.frame.setPreferredSize(new Dimension(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT));
+        this.frame.setMaximumSize(new Dimension(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT));
+        this.frame.setMinimumSize(new Dimension(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT));
+
+        this.frame.setTitle("Pathfinding demo");
+
+        this.frame.setVisible(true);
+        this.frame.setLocationRelativeTo(null);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.add(this.main);
+        this.frame.pack();
+    }
+
+
+}
